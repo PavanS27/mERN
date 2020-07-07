c607f83b303458c1281469b45a3a9ab3eee13187
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-07-07 08:13:05 +0530

    backend

---
+++ server/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb://Pavan:fsURxAn7HU9DjCRd@cluster0-shard-00-00.asv7b.mongodb.net:27017,cluster0-shard-00-01.asv7b.mongodb.net:27017,cluster0-shard-00-02.asv7b.mongodb.net:27017/mern?ssl=true&replicaSet=atlas-tsor4f-shard-0&authSource=admin&retryWrites=true&w=majority",
+  JWT_SECRET: "bcunajvguanb",
+};
